580316d85f607b61da09445aca35b3d6efbfddcf
Kevin Edmondson <kevinedmondson@Kevins-MacBook-Pro.local>
2019-01-28 16:25:23 -0600

    final

--- src/components/WizardNine/WizardNine.js
+++ src/components/WizardNine/WizardNine.js
@@ -1,25 +1,48 @@
-import React,  { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { addressOne, addressTwo, addressThree } from "../../ducks/reducer";
 
 class WizardNine extends Component {
-
-    render(){
-        return(
-            <div className="parent-div">
-                <div className="vert-align">                    
-                    
-                    <p>What is your address?</p> <br />
-
-                    <input type="text" placeholder="Line One" onChange={this.props.updateAddLineOne}/>
-                    <input type="text" placeholder="Line Two" onChange={this.props.updateAddLineTwo}/>
-                    <input type="text" placeholder="Line Three" onChange={this.props.updateAddLineThree}/>
-                
-                    
-                    <Link to="/wTen"><button className="margin-btn"> Next </button></Link>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { addressOne, addressTwo, addressThree } = this.props;
+    return (
+      <div className="parent-div">
+        <div className="vert-align">
+          <p>What is your address?</p> <br />
+          <input
+            type="text"
+            placeholder="Line One"
+            onChange={e => addressOne(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Line Two"
+            onChange={e => addressTwo(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Line Three"
+            onChange={e => addressThree(e.target.value)}
+          />
+          <Link to="/wTen">
+            <button className="margin-btn"> Next </button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+function mapStateToProps(state) {
+  const { addressOne, addressTwo, addressThree } = state;
+  return {
+    addressOne,
+    addressTwo,
+    addressThree
+  };
 }
 
-export default WizardNine;
+export default connect(
+  mapStateToProps,
+  { addressOne, addressTwo, addressThree }
+)(WizardNine);
--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,5 +1,187 @@
-function reducer(){
+const initialState = {
+  loanType: "Home Purchase",
+  propertyType: "Single Family Home",
+  city: "",
+  propToBeUsedOn: "",
+  found: false,
+  realEstateAgent: "false",
+  cost: 0,
+  downPayment: 0,
+  credit: "",
+  history: "",
+  addressOne: "",
+  addressTwo: "",
+  addressThree: "",
+  firstName: "",
+  lastName: "",
+  email: ""
+};
+const UPDATE_LOAN_TYPE = "UPDATE_LOAN_TYPE";
+const UPDATE_PROPERTY_TYPE = "UPDATE_PROPERTY_TYPE";
+const UPDATE_CITY = "UPDATE_CITY";
+const UPDATE_PROP = "UPDATE_PROP";
+const UPDATE_FOUND = "UPDATE_FOUND";
+const REAL_ESTATE_AGENT = "REAL_ESTATE_AGENT";
+const COST = "COST";
+const DOWN_PAYMENT = "";
+const CREDIT = "CREDIT";
+const HISTORY = "HISTORY";
+const ADDRESS_ONE = "ADDRESS_ONE";
+const ADDRESS_TWO = "ADDRESS_TWO";
+const ADDRESS_THREE = "ADDRESS_THREE";
+const FIRST_NAME = "FIRST_NAME";
+const LAST_NAME = "LAST_NAME";
+const EMAIL = "EMAIL";
 
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case UPDATE_LOAN_TYPE:
+      return Object.assign({}, state, { loanType: action.payload });
+
+    case UPDATE_PROPERTY_TYPE:
+      return Object.assign({}, state, { propertyType: action.payload });
+
+    case UPDATE_CITY:
+      return Object.assign({}, state, { city: action.payload });
+
+    case UPDATE_PROP:
+      return Object.assign({}, state, { prop: action.payload });
+
+    case UPDATE_FOUND:
+      return Object.assign({}, state, { found: action.payload });
+
+    case REAL_ESTATE_AGENT:
+      return Object.assign({}, state, { agent: action.payload });
+
+    case COST:
+      return Object.assign({}, state, { cost: action.payload });
+
+    case DOWN_PAYMENT:
+      return Object.assign({}, state, { payment: action.payload });
+
+    case CREDIT:
+      return Object.assign({}, state, { credit: action.payload });
+
+    case HISTORY:
+      return Object.assign({}, state, { history: action.payload });
+
+    case ADDRESS_ONE:
+      return Object.assign({}, state, { address: action.payload });
+
+    case ADDRESS_TWO:
+      return Object.assign({}, state, { address: action.payload });
+
+    case ADDRESS_THREE:
+      return Object.assign({}, state, { address: action.payload });
+
+    case FIRST_NAME:
+      return Object.assign({}, state, { first: action.payload });
+
+    case LAST_NAME:
+      return Object.assign({}, state, { last: action.payload });
+
+    case EMAIL:
+      return Object.assign({}, state, { email: action.payload });
+
+    default:
+      return state;
+  }
+}
+export function updateLoanType(loanType) {
+  return {
+    type: UPDATE_LOAN_TYPE,
+    payload: loanType
+  };
+}
+export function updatePropertyType(property) {
+  return {
+    type: UPDATE_PROPERTY_TYPE,
+    payload: property
+  };
+}
+export function updateCity(city) {
+  return {
+    type: UPDATE_CITY,
+    payload: city
+  };
+}
+export function updateProp(prop) {
+  return {
+    type: UPDATE_PROP,
+    payload: prop
+  };
+}
+export function updateFound(found) {
+  return {
+    type: UPDATE_FOUND,
+    payload: found
+  };
+}
+export function updateAgent(agent) {
+  return {
+    type: REAL_ESTATE_AGENT,
+    payload: agent
+  };
+}
+export function cost(cost) {
+  return {
+    type: COST,
+    payload: cost
+  };
+}
+export function downPayment(payment) {
+  return {
+    type: DOWN_PAYMENT,
+    payload: payment
+  };
+}
+export function credit(credit) {
+  return {
+    type: CREDIT,
+    payload: credit
+  };
+}
+export function history(history) {
+  return {
+    type: HISTORY,
+    payload: history
+  };
+}
+export function addressOne(address) {
+  return {
+    type: ADDRESS_ONE,
+    payload: address
+  };
+}
+export function addressTwo(address) {
+  return {
+    type: ADDRESS_TWO,
+    payload: address
+  };
+}
+export function addressThree(address) {
+  return {
+    type: ADDRESS_THREE,
+    payload: address
+  };
+}
+export function firstName(first) {
+  return {
+    type: FIRST_NAME,
+    payload: first
+  };
+}
+export function lastName(last) {
+  return {
+    type: LAST_NAME,
+    payload: last
+  };
+}
+export function email(email) {
+  return {
+    type: EMAIL,
+    payload: email
+  };
 }
 
 export default reducer;
